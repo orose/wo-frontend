b2e764a49a6fe4c8c254dd615918cb6de060705f
Øystein Rose <oystein.rose@gmail.com>
2018-09-05 23:15:19 +0200

    Added morgan for logging

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const os = require('os');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) =>
   res.send({ username: os.userInfo().username }),
