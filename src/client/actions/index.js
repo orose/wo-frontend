a0c21fbe478ddc922b70908499775273838c308d
Øystein Rose <oystein.rose@gmail.com>
2019-09-01 11:35:05 +0200

    Login form with redux - not completed

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,119 +1,55 @@
-export const WORKORDERS_REQUEST = "WORKORDERS_REQUEST";
-export const WORKORDERS_RECEIVE = "WORKORDERS_RECEIVE";
-export const CUSTOMERS_REQUEST = "CUSTOMERS_REQUEST";
-export const CUSTOMERS_RECEIVE = "CUSTOMERS_RECEIVE";
-export const UPDATE_PAGE_TITLE = "UPDATE_PAGE_TITLE";
-
 export const LOGIN_REQUEST = "LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 export function login(username, password) {
-  let credentials = JSON.stringify({
+  let body = JSON.stringify({
     email: username,
     password: password
   });
-  console.log("action", credentials);
   return dispatch => {
     dispatch(loginRequest());
     fetch("/api/login", {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      method: "POST",
       headers: {
         "Content-Type": "application/json"
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: credentials
+      body: body
     })
       .then(function(response) {
-        if (response.status === 200) {
-          console.log("OK");
+        if (response.ok) {
           return response.json();
         } else {
-          console.error("Authentication failed");
+          throw Error(`Request rejected with status ${response.status}`);
         }
       })
       .then(function(response) {
         dispatch(loginSuccess(response));
-        if (response !== undefined) console.log(response);
       })
-      .catch(error => console.error(error));
-    /*
-    dispatch(requestWorkorders());
-    return fetch("/api/workorders")
-      .then(response => response.json())
-      .then(json => dispatch(receiveWorkorders(json)));
-      */
+      .catch(function(error) {
+        dispatch(loginFailure());
+      });
   };
 }
 
 function loginRequest() {
   return {
-    type: LOGIN_REQUEST
+    type: LOGIN_REQUEST,
+    isFetching: true
   };
 }
 
 function loginSuccess(json) {
   return {
     type: LOGIN_SUCCESS,
-    token: json
+    isFetching: false,
+    data: json
   };
 }
 
 function loginFailure() {
   return {
-    type: LOGIN_FAILURE
-  };
-}
-
-function requestWorkorders() {
-  return {
-    type: WORKORDERS_REQUEST
-  };
-}
-
-function receiveWorkorders(json) {
-  return {
-    type: WORKORDERS_RECEIVE,
-    workorders: json,
-    receivedAt: Date.now()
-  };
-}
-
-export function fetchWorkorders() {
-  return dispatch => {
-    dispatch(requestWorkorders());
-    return fetch("/api/workorders")
-      .then(response => response.json())
-      .then(json => dispatch(receiveWorkorders(json)));
-  };
-}
-
-function requestCustomers() {
-  return {
-    type: CUSTOMERS_REQUEST
-  };
-}
-
-function receiveCustomers(json) {
-  return {
-    type: CUSTOMERS_RECEIVE,
-    customers: json,
-    receivedAt: Date.now()
-  };
-}
-
-export function fetchCustomers() {
-  return dispatch => {
-    dispatch(requestCustomers());
-    return fetch("/api/customers")
-      .then(response => response.json())
-      .then(json => dispatch(receiveCustomers(json)));
-  };
-}
-
-export function updatePageTitle(title) {
-  return {
-    type: UPDATE_PAGE_TITLE,
-    title: title
+    type: LOGIN_FAILURE,
+    isFetching: false
   };
 }
